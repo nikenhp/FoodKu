6bfb89802767bccc4c754525bef96b3fc242d64f
Niken <elfnikenhp@gmail.com>
2018-05-26 01:00:21 +0700

    Navigation Drawer

---
+++ app/src/main/java/com/example/user/foodku/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package com.example.user.foodku;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, SliderActivity.class));
+                finish();
+            }
+        }, 3000L);
+    }
+}
